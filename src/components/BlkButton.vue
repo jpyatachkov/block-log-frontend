2448cdff3ae689b74287575af6e3ef49176d1b8b
jpyatachkov <cvkucherov@yandex.ru>
2019-04-01 01:50:44 +0300

    Button white colors

--- src/components/BlkButton.vue
+++ src/components/BlkButton.vue
@@ -39,8 +39,35 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
 .round {
-  border-radius: 20px;
+  border-radius: 22px;
+}
+
+.btn-primary,
+.btn-primary:active,
+.btn-primary:hover,
+.btn-outline-primary:hover,
+.btn-secondary,
+.btn-secondary:active,
+.btn-secondary:hover,
+.btn-outline-secondary:hover,
+.btn-success,
+.btn-success:active,
+.btn-success:hover,
+.btn-outline-success:hover,
+.btn-danger,
+.btn-danger:active,
+.btn-danger:hover,
+.btn-outline-danger:hover,
+.btn-warning,
+.btn-warning:active,
+.btn-warning:hover,
+.btn-outline-warning:hover,
+.btn-info,
+.btn-info:active,
+.btn-info:hover,
+.btn-outline-info:hover {
+  color: white;
 }
 </style>
