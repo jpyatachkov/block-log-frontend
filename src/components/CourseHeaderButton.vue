5c0bbcc92c1be018b514ce1d8a4709ed1e8ff7d1
jpyatachkov <cvkucherov@yandex.ru>
2019-04-24 18:21:39 +0300

    Course controls

--- src/components/CourseHeaderButton.vue
+++ src/components/CourseHeaderButton.vue
@@ -1,19 +1,136 @@
 <template>
-  <blk-button
-  variant="success"
-  @click="onClick"
-  >
-    Пройти курс
-  </blk-button>
+  <div class="d-flex justify-content-center align-items-center">
+    <blk-button
+    v-if="userIsEnrolled"
+    :disabled="loading"
+    :loading="loading"
+    variant="success"
+    @click="onGoOn"
+    >
+      {{ goOnButtonText }}
+    </blk-button>
+    <blk-button
+    v-else
+    :disabled="loading"
+    :loading="loading"
+    variant="success"
+    @click="onEnroll"
+    >
+      {{ enrollButtonText }}
+    </blk-button>
+
+    <b-dropdown
+    v-if="userIsModerator"
+    class="ml-1"
+    variant="light"
+    >
+      <b-dropdown-item @click="showConfirmDialog = true">
+        Удалить курс
+      </b-dropdown-item>
+    </b-dropdown>
+
+    <b-modal
+    v-model="showConfirmDialog"
+    busy
+    centered
+    title="Удалить курс?"
+    >
+      <p class="my-4">
+        Это действие приведет к безвозвратному удалению курса. Вы уверены?
+      </p>
+
+      <blk-form-buttons slot="modal-footer">
+        <blk-button
+        :disabled="loading"
+        class="mr-2"
+        @click="showConfirmDialog = false"
+        >
+          Отмена
+        </blk-button>
+
+        <blk-button
+        :disabled="loading"
+        :loading="loading"
+        variant="danger"
+        @click="onDelete"
+        >
+          Удалить
+        </blk-button>
+      </blk-form-buttons>
+    </b-modal>
+  </div>
 </template>
 
 <script>
+import {
+  accountComputed,
+  coursesComputed,
+  coursesMethods,
+} from '@/store/helpers';
+
+import { LoadingMixin } from '@/mixins';
+
 export default {
   name: 'CourseHeaderButton',
 
+  mixins: [LoadingMixin],
+
+  data: () => ({
+    showConfirmDialog: false,
+  }),
+
+  computed: {
+    ...accountComputed,
+    ...coursesComputed,
+
+    enrollButtonText() {
+      return this.userCanCreateCourses ? 'Преподавать курс' : 'Пройти курс';
+    },
+
+    goOnButtonText() {
+      return 'Продолжить';
+    },
+  },
+
   methods: {
-    onClick() {
-      console.log('ENROLL');
+    ...coursesMethods,
+
+    async onEnroll() {
+      const courseId = this.$route.params.id;
+
+      this.setLoading(true);
+
+      await this.enrollCourse({ courseId });
+      // Нужно для того, чтобы список курсов обновился.
+      // Если этого не сделать, то при переходе на страницу моих курсов или курсов в целом
+      // не будет запроса к API (т.к. мы кэшируем извлеченные данные, чтобы не делать лишних запросов),
+      // соответственно, нужно будет обновлять страницу вручную, чтобы увидеть созданный курс.
+      this.clearMyCourses();
+
+      this.setLoading(false);
+    },
+
+    onGoOn() {
+      // TODO: Сделать перекидывание на список заданий.
+    },
+
+    async onDelete() {
+      const courseId = this.$route.params.id;
+
+      this.setLoading(true);
+
+      await this.deleteCourse({ courseId });
+      // Нужно для того, чтобы список курсов обновился.
+      // Если этого не сделать, то при переходе на страницу моих курсов или курсов в целом
+      // не будет запроса к API (т.к. мы кэшируем извлеченные данные, чтобы не делать лишних запросов),
+      // соответственно, нужно будет обновлять страницу вручную, чтобы увидеть созданный курс.
+      this.clearCourses();
+      this.clearMyCourses();
+
+      this.setLoading(false);
+
+      this.showConfirmDialog = false;
+      this.$router.push({ name: 'my_courses' });
     },
   },
 };
