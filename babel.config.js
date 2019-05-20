c1810d58dcecb6e18bb435a6e0cfb2b3fd2557c1
jpyatachkov <cvkucherov@yandex.ru>
2019-05-20 18:08:25 +0300

    Remove console.log

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,10 @@
+const plugins = [];
+
+if (process.env.NODE_ENV === 'production') {
+  plugins.push('transform-remove-console');
+}
+
 module.exports = {
-  presets: ['@babel/preset-env',
-    '@vue/app'],
+  presets: ['@babel/preset-env', '@vue/app'],
+  plugins,
 };
